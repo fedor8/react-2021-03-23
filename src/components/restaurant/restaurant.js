f60cd358e3c3dfebd3d05041f1201b21503770c1
Fedor Dydykin <fedor.dydykin@asseco.pl>
2021-04-15 17:18:07 +0300

    DZ1 restaurant function refactoring

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -3,14 +3,14 @@ import Rate from "../rate";
 import {useMemo} from "react";
 import {ReviewList} from "../review";
 
+const Restaurant = ({restaurant}) => {
 
-export default function Restaurant(props) {
-
-    const reviews = props.restaurant.reviews;
+    const {reviews, menu} = restaurant;
 
     const activeRestaurantRating = useMemo(
         () => {
-            return reviews.reduce((sum, review) => review.rating + sum, 0) / reviews.length;
+            const ratingSum = reviews.reduce((sum, review) => review.rating + sum, 0);
+            return ratingSum / reviews.length;
         },
         [reviews]
     );
@@ -20,9 +20,11 @@ export default function Restaurant(props) {
             <h3>Рейтинг</h3>
             <Rate value={activeRestaurantRating.toFixed(2)}/>
             <h3>Меню</h3>
-            <Menu menu={props.restaurant.menu}/>
+            <Menu menu={menu}/>
             <h3>Отзывы</h3>
             <ReviewList reviews={reviews}/>
         </div>
     );
 }
+
+export default Restaurant;
